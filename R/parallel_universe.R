7d458c33c040cb9eb8e1cc772f931250b1238cf2
Jane Lange <you@example.com>
2025-11-13 15:59:03 -0800

    test

--- R/parallel_universe.R
+++ R/parallel_universe.R
@@ -74,6 +74,9 @@ sim_individual_MCED<-function( ID,
   # Add other-cause death info
   result$other_cause_death_status <- other_cause_death$status
   result$other_cause_death_time <- other_cause_death$time
+  if(result$other_cause_death_time<starting_age){
+    browser()
+  }
   result$sex=sex
   #-----------------------  
   # Identify first cancer by onset time
